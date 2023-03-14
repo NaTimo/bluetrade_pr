e7a837dd42c23cdd247d16293141057cabaca79c
Naglis Timofejevas <timofejevasnaglis@gmail.com>
2023-03-14 06:32:22 +0200

    3

--- bluetrade/bluetrade_app/urls.py
+++ bluetrade/bluetrade_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.ads, name='home'),
     path('<int:ad_id>/', views.ad, name='ad'),
+    path("", views.AdListView.as_view(), name="home")
 ]
--- bluetrade/bluetrade_app/views.py
+++ bluetrade/bluetrade_app/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Ad
+from django.views import generic
 
 def ads(request):
     ads = Ad.objects.all()
+    peginate_by = 10
     context = {
         "ads": ads,
     }
     return render(request, "home.html", context=context)
 
+class AdListView(generic.ListView):
+    model = Ad
+    template_name = "home.html"
+    context_object_name = "ads"
 def ad(request, ad_id):
     ad = get_object_or_404(Ad, pk=ad_id)
     context = {
